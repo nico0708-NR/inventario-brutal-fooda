2c9a4a21263e0d8e469586f2631e83e0a83b6b32
nico0708-NR <nicorodricues@gmail.com>
2026-05-29 23:31:57 +0200

    actualizado

--- app.py
+++ app.py
@@ -491,4 +491,5 @@ def ventas():
 # EJECUTAR APP
 # =========================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000) 
+    print("BRUTAL FOOD ACTUALIZADO")
